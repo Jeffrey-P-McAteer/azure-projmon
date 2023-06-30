1d137980b4d4c78018a8b9827dd4a5731b1692d5
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2023-06-30 19:22:34 -0400

    notes

---
+++ os_setup.md
@@ -0,0 +1,9 @@
+
+```bash
+yay -S evdi
+
+sudo vim /etc/modules-load.d/evdi.conf
+# Add "evdi" and "options evdi initial_device_count=1"
+
+
+```
--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,7 @@ const PROJECTOR_DISPLAY_NAMES:  &'static [&'static str] = &[
 
 
 async fn async_main(args: Vec<String>) {
+  let exit_signal_handler = tokio::task::spawn(handle_exit_signals());
 
   let mut sway_conn = dump_error_and_ret!( swayipc_async::Connection::new().await );
 
@@ -54,21 +55,117 @@ async fn async_main(args: Vec<String>) {
     if connected_projector_name.len() > 1 {
       break; // found projector!
     }
+
+    if EXIT_FLAG.load(std::sync::atomic::Ordering::SeqCst) {
+      EXIT_TASKS_DONE.store(true, std::sync::atomic::Ordering::SeqCst);
+      return; // no cleanup yet
+    }
     
 
-    tokio::time::sleep( std::time::Duration::from_millis(850) ).await;    
+    tokio::time::sleep( std::time::Duration::from_millis(250) ).await;
   }
 
   println!("");
   println!("Saw connected projector at {:?} with workspace {:?}", &connected_projector_name, &connected_projector_ws);
 
+  // Set black wallpaper + virtual location to -10 screens to the left (sway prevents mouse from jumping! \o/)
+  dump_error!( sway_conn.run_command(format!("output {} mode 1920x1080 pos -19200 0 bg #000000 solid_color", &connected_projector_name).as_str()).await );
 
 
   // Once projector is connected, create EVDI virtual monitor & tell sway to position at -1080,0
 
+  let evidi_handle = unsafe { evdi_sys::evdi_open_attached_to( std::ptr::null() ) };
+  if evidi_handle.is_null() {
+    eprintln!("Got null pointer from evdi_sys::evdi_open_attached_to!");
+    return;
+  }
 
+  // 1920x1080 * 3 bytes/pixel = 
+  //let mut evidi_screen_mem: [u8; 6220800] = [0; 6220800]; // blows stack
+  let mut evidi_screen_mem = vec![0_u8; 6220800]; // Directly allocated on heap
+
+  let evidi_buff = evdi_sys::evdi_buffer {
+    id: 0,
+    buffer: evidi_screen_mem.as_mut_ptr() as *mut core::ffi::c_void,
+    width: 1920,
+    height: 1080,
+    stride: 3,
+    rects:  std::ptr::null_mut::<evdi_sys::evdi_rect>(), // these 2 are modified by EVDI runtime to inform us of damaged areas to re-paint.
+    rect_count: 0,
+  };
+
+  unsafe { evdi_sys::evdi_connect(evidi_handle, std::ptr::null(), 0, 6220800 ) };
+
+  tokio::time::sleep( std::time::Duration::from_millis(250) ).await;
+
+  // Print sway displays
+  if let Ok(outputs) = sway_conn.get_outputs().await {
+    for output in outputs.iter() {
+      println!("");
+      println!("output = {:?}", output);
+      println!("");
+    }
+  }
+
+  tokio::time::sleep( std::time::Duration::from_millis(250) ).await;
+
+  // Finally close it
+  unsafe { evdi_sys::evdi_disconnect(evidi_handle); }
+  unsafe { evdi_sys::evdi_close(evidi_handle) };
 
 
   println!("Done!");
 
+  EXIT_TASKS_DONE.store(true, std::sync::atomic::Ordering::SeqCst);
+
 }
+
+static EXIT_FLAG: once_cell::sync::Lazy<std::sync::atomic::AtomicBool> = once_cell::sync::Lazy::new(||
+  std::sync::atomic::AtomicBool::new(false)
+);
+
+static EXIT_TASKS_DONE: once_cell::sync::Lazy<std::sync::atomic::AtomicBool> = once_cell::sync::Lazy::new(||
+  std::sync::atomic::AtomicBool::new(false)
+);
+
+async fn handle_exit_signals() {
+  let mut int_stream = dump_error_and_ret!(
+    tokio::signal::unix::signal(
+      tokio::signal::unix::SignalKind::interrupt()
+    )
+  );
+  let mut term_stream = dump_error_and_ret!(
+    tokio::signal::unix::signal(
+      tokio::signal::unix::SignalKind::terminate()
+    )
+  );
+  loop {
+    let mut want_shutdown = false;
+    tokio::select!{
+      _sig_int = int_stream.recv() => { want_shutdown = true; }
+      _sig_term = term_stream.recv() => { want_shutdown = true; }
+    };
+    if want_shutdown {
+      println!("Got SIG{{TERM/INT}}, shutting down!");
+      
+      EXIT_FLAG.store(true, std::sync::atomic::Ordering::SeqCst);
+
+      // Allow spawned futures to complete...
+      for _ in 0..20 { // 50ms/poll * 20 == 1000ms max wait
+        if EXIT_TASKS_DONE.load(std::sync::atomic::Ordering::SeqCst) {
+          break;
+        }
+        tokio::time::sleep( tokio::time::Duration::from_millis(50) ).await;
+      }
+
+      println!("Goodbye!");
+      std::process::exit(0);
+    }
+  }
+}
+
+
+
+
+
+
